d6ee50f09871f1414d38085070662e4cb812a242
smhabib99-99 <habibaust34@gmail.com>
2023-11-28 21:14:38 +0600

    Join as a Donor redirect to registration page implementation is successful

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Banner = () => {
@@ -8,12 +9,15 @@ const Banner = () => {
                     <img className='h-96 w-full' src="/banner.avif" alt="" />
                 </div>
                 <div className="absolute top-72 right-4 left-10 z-10">
-                    <div className='items-center text-center'>
-                        <input type="search" name="" placeholder='Search donor here....' className='border py-2 pl-6 relative rounded-lg left-1 w-full max-w-xl' id="" />
-                        <button className='bg-red-400 rounded-r-lg absolute py-2 px-3 text-white'>Search Donors</button>
+                  
+                    <div className="flex justify-center">
+                    <button className="bg-red-400 rounded-md text-white font-bold text-xl p-2">Search Donors</button>
                     </div>
                     <div className="flex justify-center">
-                    <button className="bg-red-400 rounded-md text-white font-bold text-xl p-2 my-4">Join as Donor</button>
+                        <Link to='/registration'>
+                        <button className="bg-red-400 rounded-md text-white font-bold text-xl p-2 my-4">Join as a Donor</button>
+                        </Link>
+                    
                     </div>
                 </div>
             </div>
